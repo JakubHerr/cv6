e7f08f49f16f6da796834294e9de3cc63301d3d3
Jakub Herrmann <herj17@vse.cz>
2023-03-21 16:03:47 +0100

    Initial commit

---
+++ src/main/java/com/example/cv6bonus/Cv6bonusApplication.java
@@ -0,0 +1,13 @@
+package com.example.cv6bonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv6bonusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cv6bonusApplication.class, args);
+    }
+
+}
